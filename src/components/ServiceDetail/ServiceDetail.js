7e6575e0df51d19b77e6a4f00f103cdfeb6236b0
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-10-05 19:33:22 +0600

    service-details and some style update

--- src/components/ServiceDetail/ServiceDetail.js
+++ src/components/ServiceDetail/ServiceDetail.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import useServices from '../../hooks/useServices';
 import { useParams } from 'react-router';
 
 const ServiceDetail = () => {
 	const {serviceId} = useParams();
-	const myServices = useServices();
-	console.log(myServices);
 	
 	return (
 		<div className="pt-32 pb-14">
 			<div className="container">
-				<h2>Service Detail: {serviceId}</h2>
+				<h2 className="text-center">Service Detail : <span className="text-my-primary text-2xl font-body">{serviceId}</span></h2>
 			</div>
 		</div>
 	);
